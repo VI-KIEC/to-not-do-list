a7c542942ef294548c7463e1cf69d2c564348549
VI-KIEC <noodle97211293@gmail.com>
2023-08-23 03:11:13 +0900

    . Dropした後にタスクカードを並び替える機能

--- src/components/task/TaskCards.jsx
+++ src/components/task/TaskCards.jsx
@@ -4,12 +4,23 @@ import AddTaskCardButton from "./ button/AddTaskCardButton";
 import TaskCard from "./TaskCard";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
+const reorder = (taskCardsList, startIndex, endIndex) => {
+  // タスクを並び替える
+  const remove = taskCardsList.splice(startIndex, 1);
+  taskCardsList.splice(endIndex, 0, remove[0]);
+};
 const TaskCards = () => {
   const [taskCardsList, setTaskCardsList] = useState([
     { id: "0", draggableId: "item0" },
   ]);
+
+  const handleDragEnd = (result) => {
+    reorder(taskCardsList, result.source.index, result.destination.index);
+
+    setTaskCardsList(taskCardsList);
+  };
   return (
-    <DragDropContext>
+    <DragDropContext onDragEnd={handleDragEnd}>
       <Droppable droppableId="droppable" direction="horizontal">
         {(provided) => (
           <div
