8c32954ceb7b7076f0b9e0e699746b5dfdfe9818
VI-KIEC <noodle97211293@gmail.com>
2023-08-24 19:03:33 +0900

    ２カード目完成

--- src/components/taskCardB/TaskCard2AddInput.jsx
+++ src/components/taskCardB/TaskCard2AddInput.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 
-const TaskCard2AddInput = () => {
+const TaskCard2AddInput = ({
+  inputText,
+  setInputText,
+  taskList,
+  setTaskList,
+}) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // カードを追加する
+    if (inputText === "") {
+      return;
+    }
+    setTaskList([...taskList, { text: inputText, id: taskList.length }]);
+    setInputText("");
+  };
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+  };
   return (
     <div>
-      <form>
-        <input type="text" placeholder="add a task" className="taskAddInput" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="add a task"
+          className="taskAddInput"
+          onChange={handleChange}
+          value={inputText}
+        />
       </form>
     </div>
   );
